61adab62fb8ed30448164b484500706d0f323c5b
Filipe Santos <filipesantos.py@gmail.com>
2023-09-26 09:07:37 -0300

    refatorando novamente e removendo botão

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-document.getElementById("btnRefresh").addEventListener("click", function() {
-    rollDice()
-    //location.reload()
-})
+var randomNumber1 = Math.floor(Math.random() * 6 + 1)
+var randomNumber2 = Math.floor(Math.random() * 6 + 1)
 
-
-function rollDice(){
-    var randomNumber1 = Math.floor(Math.random() * 6 + 1)
-    var randomNumber2 = Math.floor(Math.random() * 6 + 1)
-
-    var tittle = document.querySelector("h1")
-    if (randomNumber1 === randomNumber2){
-        tittle.innerHTML = "Empate 😔"
-    } else if (randomNumber1 > randomNumber2){
-        tittle.innerHTML = "Jogador 1 ganhou 🎉"
-    } else {
-        tittle.innerHTML = "Jogador 2 ganhou 🎉"
-    }
-
-    //faz a alteração da imagem de acordo com o numero pseudo aleatorio que é gerado
-    document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png")
-    document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png")
+var tittle = document.querySelector("h1")
+if (randomNumber1 === randomNumber2){
+    tittle.innerHTML = "Empate 😔"
+} else if (randomNumber1 > randomNumber2){
+    tittle.innerHTML = "Jogador 1 ganhou 🎉"
+} else {
+    tittle.innerHTML = "Jogador 2 ganhou 🎉"
 }
+
+//faz a alteração da imagem de acordo com o numero pseudo aleatorio que é gerado
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png")
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png")
